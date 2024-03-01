8747bb229221220db42473ef6b32436a95ba526f
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-03-02 00:33:31 +0200

    Update 416. Partition Equal Subset Sum.cpp

--- LeetCode Problems/416. Partition Equal Subset Sum.cpp
+++ LeetCode Problems/416. Partition Equal Subset Sum.cpp
@@ -1,19 +1,34 @@
+const static auto _ = [] { std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr); return nullptr; }();
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int totalSum = accumulate(nums.begin(), nums.end(), 0);
-        if (totalSum % 2 != 0) 
+int dp[200 + 1][10000 + 4];
+int n;
+vector<int>v;
+int tot;
+bool can(int i, int sum){
+    if(sum > tot / 2)
+        return false;
+    if(sum == tot - sum)
+        return true;
+    if(i == n)
+        return false;
+    int &ret = dp[i][sum];
+    if(~ret)
+        return ret;
+    bool op1 = can( i + 1, sum);
+    bool op2 = can(i + 1, sum + v[i]) ;
+    return ret = op1||op2;
+}
+bool canPartition(vector<int>& nums) {
+    n = nums.size();
+    v = nums;
+    tot = 0;
+    for(int i = 0 ; i < n ; i++)
+        tot+=v[i];
+        if(tot & 1)
             return false;
-        
-        int target = totalSum / 2;
-        vector<bool> dp(target + 1, false);
-        dp[0] = true;
-        
-        for (int num : nums) {
-            for (int j = target; j >= num; --j) {
-                dp[j] = dp[j] || dp[j - num];
-            }
-        }
-        return dp[target];
-    }
+    memset(dp, -1, sizeof dp);
+   return can(0,0);
+}
+
 };
