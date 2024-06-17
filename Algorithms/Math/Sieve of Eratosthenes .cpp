a16d4ae4a3107ec0cf5e2a488f5269f562b99797
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-06-17 14:41:13 +0300

    Update Sieve of Eratosthenes .cpp

--- Algorithms/Math/Sieve of Eratosthenes .cpp
+++ Algorithms/Math/Sieve of Eratosthenes .cpp
@@ -1,14 +1,12 @@
-vector<int>primes;
-vector<bool>composite(1e6);
-
-void sieveOfEratosthenes(){
-    composite[0]=composite[1]=true;
-
-    for(int i = 2 ; i * i < composite.size() ; i++){
-        if(!composite[i]){
-            for(int j = i * i ; j <= composite.size() ; j+=i){
-                composite[j] = true;
-            }
+vector<bool>not_primes(1e6 + 1);vector<int>primes;
+void sieve(int n){
+    primes.clear();
+    not_primes[0] = not_primes[1] = true;
+    for(int i = 2 ; i<= n ; i++){
+        if(!not_primes[i]){
+            primes.push_back(i);
+            for(int j = i * i ; j <= n ; j += i)
+                not_primes[j] = true;
         }
     }
 }
