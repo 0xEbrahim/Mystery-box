1667a617fbbd75b8d76f7e12ccd35aa7a68de45f
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-06-27 14:29:41 +0300

    Create Detect_Cycle_Undirected_Graph.cpp

---
+++ Algorithms/Algorithms At All/Detect_Cycle_Undirected_Graph.cpp
@@ -0,0 +1,17 @@
+vector<vector<int>>adj;
+vector<bool>vis;
+bool detect_cycle(int node, int parent){
+    vis[node] = true;
+    for(auto &child : adj[node]){
+        if(!vis[child]) {
+            if (detect_cycle(child, node))
+                return true;
+        }else {
+            if (child != parent)
+                return true;
+            else
+                return false;
+        }
+    }
+    return false;
+}
