7ec60f548b31720fc4d2a71d5c5487c7637bd525
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2025-02-05 18:25:12 +0200

    Update DSU.cpp

--- Data Structures/DSU.cpp
+++ Data Structures/DSU.cpp
@@ -1,9 +1,13 @@
 struct DSU {
     vector<int>parent;
     vector<int>sz;
+    vector<int>sum;
+    int comps;
     void init(int n){
         parent.resize(n + 5);
         sz.resize(n + 5);
+        sum.resize(n + 5);
+        comps = n;
         for (int i = 1; i <= n; ++i) {
             parent[i] = i;
             sz[i] = 1;
@@ -17,6 +21,9 @@ struct DSU {
     bool is_connected(int u, int v){
         return find(u) == find(v);
     }
+    int get_sum(int u){
+        return sum[find(u)];
+    }
     void merge(int u , int v){
         int root_u = find(u);
         int root_v = find(v);
@@ -24,7 +31,9 @@ struct DSU {
             return;
         if(sz[root_u] > sz[root_v])
             swap(root_u, root_v);
+        comps--;
         parent[root_u] = root_v;
         sz[root_v] += sz[root_u];
+        sum[root_v]+=sum[root_u];
    }
 };
