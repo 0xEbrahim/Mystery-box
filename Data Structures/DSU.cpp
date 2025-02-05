44dd3638bd20dbc7af71944b65597ef295f30620
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2025-02-05 17:45:50 +0200

    Update DSU.cpp

--- Data Structures/DSU.cpp
+++ Data Structures/DSU.cpp
@@ -1,39 +1,30 @@
-struct dsu {
-    vector<int> par, size;
-    int cnt;
-    void init(int n) {
-        cnt = 0;
-        par.resize(n + 1, -1);
-        size.resize(n + 1, 0);
+struct DSU {
+    vector<int>parent;
+    vector<int>sz;
+    void init(int n){
+        parent.resize(n + 5);
+        sz.resize(n + 5);
+        for (int i = 1; i <= n; ++i) {
+            parent[i] = i;
+            sz[i] = 1;
+        }
     }
-    void init_all(int n) {
-        cnt = n;
-        par.resize(n + 1);
-        size.assign(n + 1, 1);
-        for (int i = 1; i <= n; ++i) par[i] = i;
+    int find(int u){
+        if(u == parent[u])
+            return u;
+       return parent[u] = find(parent[u]);
     }
-    int find_root(int u) {
-        if (par[u] == u) return u;
-        return par[u] = find_root(par[u]);
+    bool is_connected(int u, int v){
+        return find(u) == find(v);
     }
-    bool is_connected(int u, int v) {
-        return find_root(u) == find_root(v);
-    }
-    void make_set(int u) {
-        if (~par[u]) return;
-        par[u] = u;
-        cnt++;
-        size[u] = 1;
-    }
-    bool merge(int u, int v) {
-        int root_u = find_root(u);
-        int root_v = find_root(v);
-        if (root_u == root_v) return 0;
-        if (size[root_u] > size[root_v])
+    void merge(int u , int v){
+        int root_u = find(u);
+        int root_v = find(v);
+        if(root_u == root_v)
+            return;
+        if(sz[root_u] > sz[root_v])
             swap(root_u, root_v);
-        size[root_v] += size[root_u];
-        par[root_u] = root_v;
-        --cnt;
-        return 1;
-       }
+        parent[root_u] = root_v;
+        sz[root_v] += sz[root_u];
+   }
 };
