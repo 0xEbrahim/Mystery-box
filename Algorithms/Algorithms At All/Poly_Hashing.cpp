deb32fb974016d30f42e8980f6dba1d0b1d30e9c
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2025-04-11 01:02:45 +0200

    Create Poly_Hashing.cpp

---
+++ Algorithms/Algorithms At All/Poly_Hashing.cpp
@@ -0,0 +1,29 @@
+struct Shash {
+    int base, inv, mod, n;
+    vector<long long>pw{1}, invpw{1}, pref{0};
+
+    Shash(const string &s, int m, int b){
+        mod = m;
+        base=b;
+        n=s.size();
+        inv = 1;
+        long long e = mod - 2;
+        long long cur = base;
+
+        while(e){
+            if(e & 1)inv = 1ll * inv * cur % mod;
+            (cur *= cur) %= mod;
+            e >>= 1;
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            pw.push_back(pw.back() * base % mod);
+            invpw.push_back(invpw.back() * inv % mod);
+            pref.push_back((pref.back() + s[i] * pw[i]) % mod);
+        }
+    }
+
+    long long get(long long l, long long r){
+        return (pref[r + 1] - pref[l] + mod) * invpw[l] % mod;
+    }
+};
