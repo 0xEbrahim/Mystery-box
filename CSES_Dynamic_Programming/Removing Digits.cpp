497033f81078a058001d8052389ae4729ebe8a23
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2025-04-20 17:06:44 +0200

    Create Removing Digits.cpp

---
+++ CSES_Dynamic_Programming/Removing Digits.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ld long double
+#define int long long
+#define endl '\n'
+#define all(v) ((v).begin()), ((v).end())
+void BRO(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+const int mod = 998244353;
+const int MOD = 1e9 + 7;
+template <typename T> void stop(T x){return void(cout << x << endl);}
+void HIRO(void) {
+#ifndef ONLINE_JUDGE
+    freopen("D:\\Personal\\No\\CP\\input.txt", "r", stdin);
+    freopen("D:\\Personal\\No\\CP\\output.txt", "w", stdout);
+#endif
+}
+ 
+ 
+void done() {
+    int n;
+    cin >> n ;
+    vector<int>dp(n + 1, LLONG_MAX);
+    dp[0] = 0;
+    for(int i = 0 ; i <= n ; i++){
+        string str = to_string(i);
+        for(auto &c:str){
+            int num = c - '0';
+            if(i - num >= 0)
+                dp[i] = min(dp[i], dp[i - num] + 1);
+        }
+    }
+    cout << dp[n];
+}
+ 
+signed main() {
+    BRO();
+    HIRO();
+    int t;
+    t = 1;
+//    cin >> t;
+    while (t--) {
+        done();
+    }
+    return 0;
+}
