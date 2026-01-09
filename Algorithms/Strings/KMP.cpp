7f44d44f1a91209b56566599f2a5ad7b241f2626
Ibrahim El-Sayed <elsayedebrahim603@gmail.com>
2026-01-09 21:31:29 +0200

    Update and rename prefix_function.cpp to KMP.cpp

---
+++ Algorithms/Strings/KMP.cpp
@@ -0,0 +1,29 @@
+class KMP {
+public:
+    vector<int> prefix_function(string &s) {
+        int n = s.size();
+        vector<int>pi(n);
+        pi[0] = 0;
+        for(int i = 1 ; i < n ; i++) {
+            int j = pi[i - 1];
+            while(j > 0 && s[j] != s[i])
+                j = pi[j - 1];
+            if(s[i] == s[j])
+                j++;
+            pi[i] = j;
+        }
+        return pi;
+    }
+
+    vector<int> count_prefix(vector<int>&pi) {
+        int n = pi.size();
+        vector<int> ans(n + 1);
+        for (int i = 0; i < n; i++)
+            ans[pi[i]]++;
+        for (int i = n-1; i > 0; i--)
+            ans[pi[i-1]] += ans[i];
+        for (int i = 0; i <= n; i++)
+            ans[i]++;
+        return ans;
+    }
+};
